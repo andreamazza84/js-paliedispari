562e09a9b41c5634f43d0e2c296d03726afd4c24
andreamazza84 <andrea.mazza84@gmail.com>
2020-11-20 15:52:29 +0100

    Esercizio palindroma completo

---
+++ palindroma/main.js
@@ -0,0 +1,25 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+function isPalindroma(word){
+  var letter = "";
+  var reverseCounter = 0;
+  var mirrorWord = "";
+  for (var i = 0; i < word.length; i++) {
+    reverseCounter = (word.length - i - 1);
+    letter = word[reverseCounter]
+    mirrorWord += letter;
+  }
+  if(word == mirrorWord){
+    return true;
+  };
+}
+
+var userWord = prompt("Inserisci una parola");
+if (isPalindroma(userWord)) {
+  alert("La parola inserita è palindroma");
+}
+else{
+  alert("La parola inserita NON è palindroma")
+}
